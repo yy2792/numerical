f8bc9f3308dc6edf1bef919d505f81fe640b39f6
yy2792 <yy2792@columbia.edu>
2019-04-20 22:35:14 -0400

    set up root solving

---
+++ root_solving/main.py
@@ -0,0 +1,111 @@
+from root_solving.BS_formula import BS_Price, BS_Vega
+import unittest
+
+
+def newton(guess, target, para_json, cp='c', max_loop=1000, thred=10**(-8)):
+
+    for i in range(max_loop):
+        para_json['vol'] = guess
+        guess_res = BS_Price(para_json, cp=cp) - target
+        guess_res_diff = BS_Vega(para_json)
+
+        if abs(guess_res) < thred:
+            return guess
+        else:
+            guess -= guess_res / guess_res_diff
+
+    return None
+
+
+def secant(guess1, guess2, target, para_json, cp='c', max_loop=1000, thred=10**(-8)):
+
+    for i in range(max_loop):
+        para_json['vol'] = guess1
+        guess_res1 = BS_Price(para_json, cp=cp) - target
+
+        para_json['vol'] = guess2
+        guess_res2 = BS_Price(para_json, cp=cp) - target
+
+        new_guess = (guess_res2 * guess1 - guess_res1 * guess2) / (guess_res2 - guess_res1)
+        para_json['vol'] = new_guess
+        new_guess_res = BS_Price(para_json, cp=cp) - target
+
+        if new_guess_res < thred:
+            return new_guess
+
+        if abs(new_guess - guess1) >= abs(new_guess - guess2):
+            guess1 = new_guess
+            guess2 = guess1
+        else:
+            guess1 = new_guess
+
+    return None
+
+
+def regula(guess1, guess2, target, para_json, cp='c', max_loop=1000, thred=10**(-8)):
+
+    for i in range(max_loop):
+        para_json['vol'] = guess1
+        guess_res1 = BS_Price(para_json, cp=cp) - target
+
+        para_json['vol'] = guess2
+        guess_res2 = BS_Price(para_json, cp=cp) - target
+
+        new_guess = (guess_res2 * guess1 - guess_res1 * guess2) / (guess_res2 - guess_res1)
+        para_json['vol'] = new_guess
+        new_guess_res = BS_Price(para_json, cp=cp) - target
+
+        if new_guess_res < thred:
+            return new_guess
+
+        if new_guess_res * guess_res1 < 0:
+            guess2 = new_guess
+        else:
+            guess1 = new_guess
+
+    return None
+
+
+
+class MyError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+class TestBS(unittest.TestCase):
+
+    def setUp(self):
+        self.price_json = {
+            'S': 120,
+            'K': 120,
+            't': 92/365,
+            'r': 0.005,
+            'q': 0.04,
+            'vol': 0.5
+        }
+
+    def test_newton(self):
+        res = newton(0.2, 1, self.price_json)
+        self.assertEqual(0.0614, round(res, 4))
+
+        res = newton(0.2, 3, self.price_json)
+        self.assertEqual(0.1465, round(res, 4))
+
+    def test_secant(self):
+        res = secant(0.19, 0.2, 1, self.price_json)
+        self.assertEqual(0.0614, round(res, 4))
+
+        res = secant(0.19, 0.2, 3, self.price_json)
+        self.assertEqual(0.1465, round(res, 4))
+
+    def test_regula(self):
+        res = regula(0.19, 0.2, 1, self.price_json)
+        self.assertEqual(0.0614, round(res, 4))
+
+        res = regula(0.19, 0.2, 3, self.price_json)
+        self.assertEqual(0.1465, round(res, 4))
+
+
